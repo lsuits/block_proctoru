e5e0a96520d5826fd71d04851c11a81e2c7f2433
Jason Peak <jpeak5@lsu.edu>
2013-09-26 13:51:17 -0500

    refactor block class to use new lib class

--- block_proctoru.php
+++ block_proctoru.php
@@ -1,4 +1,6 @@
 <?php
+global $CFG;
+require_once $CFG->dirroot . '/blocks/proctoru/lib.php';
 
 class block_proctoru extends block_base {
 
@@ -14,7 +16,7 @@ class block_proctoru extends block_base {
             'shortname' => get_string('infofield_shortname', 'block_proctoru'),
             'categoryid' => 1,
         );
-        $this->field = self::default_profile_field($fieldParams);
+        $this->field = ProctorU::default_profile_field($fieldParams);
     }
 
     public function get_content() {
@@ -25,28 +27,13 @@ class block_proctoru extends block_base {
 
         $this->content = new stdClass();
 
-        if ($this->isUserATeacherSomehwere()) {
+        if (ProctorU::isUserATeacherSomehwere()) {
             return $this->content;
         } else {
             return $this->content->text = "You must register with Proctor U before Moodle course content will be available!";
         }
     }
 
-    public function isUserATeacherSomehwere() {
-        global $CFG, $USER;
-        require_login();
-
-        $contexts = $USER->access['ra'];
-
-        foreach ($contexts as $path => $role) {
-            foreach (array_values($role) as $roleid) {
-                if (in_array($roleid, explode(',', $CFG->block_proctoru_roleselection))) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 
     public function cron() {
         global $CFG;
@@ -57,35 +44,6 @@ class block_proctoru extends block_base {
         }
         return true;
     }
-
-    /**
-     * 
-     * @global type $DB
-     * @param type $params
-     * @return \stdClass
-     */
-    public static function default_profile_field($params) {
-        global $DB;
-
-        if (!$field = $DB->get_record('user_info_field', $params)) {
-            $field = new stdClass;
-            $field->shortname = $params['shortname'];
-            $field->name = get_string($field->shortname, 'block_proctoru');
-            $field->description = get_string('custom_field_desc', 'block_proctoru');
-            $field->descriptionformat = 1;
-            $field->datatype = 'text';
-            $field->categoryid = $params['categoryid'];
-            $field->locked = 1;
-            $field->visible = 1;
-            $field->param1 = 30;
-            $field->param2 = 2048;
-
-            $field->id = $DB->insert_record('user_info_field', $field);
-        }
-
-        return $field;
-    }
-
 }
 
 ?>

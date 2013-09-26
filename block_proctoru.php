5f2832586194042529e17bb21e956e0b033ae50c
Jason Peak <jpeak5@lsu.edu>
2013-09-26 11:53:03 -0500

    adds profile field using code borrowed from CPS

--- block_proctoru.php
+++ block_proctoru.php
@@ -1,39 +1,46 @@
 <?php
 
 class block_proctoru extends block_base {
+
+    public $field;
     
-    public function has_config(){
+    public function has_config() {
         return true;
     }
+
     public function init() {
         $this->title = get_string('pluginname', 'block_proctoru');
+        $fieldParams = array(
+            'shortname' => get_string('infofield_shortname', 'block_proctoru'),
+            'categoryid' => 1,
+        );
+        $this->field = self::default_profile_field($fieldParams);
     }
-    
+
     public function get_content() {
 
         if ($this->content !== null) {
-          return $this->content;
+            return $this->content;
         }
-        
+
         $this->content = new stdClass();
 
-        if($this->isUserATeacherSomehwere()){
+        if ($this->isUserATeacherSomehwere()) {
             return $this->content;
-        }else{
+        } else {
             return $this->content->text = "You must register with Proctor U before Moodle course content will be available!";
         }
-
     }
-    
-    public function isUserATeacherSomehwere(){
-        global $CFG,$USER;
+
+    public function isUserATeacherSomehwere() {
+        global $CFG, $USER;
         require_login();
-        
+
         $contexts = $USER->access['ra'];
-        
-        foreach($contexts as $path=>$role){
-            foreach(array_values($role) as $roleid){
-                if(in_array($roleid, explode(',',$CFG->block_proctoru_roleselection))){
+
+        foreach ($contexts as $path => $role) {
+            foreach (array_values($role) as $roleid) {
+                if (in_array($roleid, explode(',', $CFG->block_proctoru_roleselection))) {
                     return true;
                 }
             }
@@ -41,15 +48,44 @@ class block_proctoru extends block_base {
         return false;
     }
 
-    public function cron(){
+    public function cron() {
         global $CFG;
-        if($CFG->block_proctoru_bool_cron == 1){
+        if ($CFG->block_proctoru_bool_cron == 1) {
             mtrace(sprintf("Running ProctorU cron tasks"));
-        }else{
+        } else {
             mtrace("Skipping ProctorU");
         }
         return true;
     }
- 
+
+    /**
+     * 
+     * @global type $DB
+     * @param type $params
+     * @return \stdClass
+     */
+    public static function default_profile_field($params) {
+        global $DB;
+
+        if (!$field = $DB->get_record('user_info_field', $params)) {
+            $field = new stdClass;
+            $field->shortname = $params['shortname'];
+            $field->name = get_string($field->shortname, 'block_proctoru');
+            $field->description = get_string('custom_field_desc', 'block_proctoru');
+            $field->descriptionformat = 1;
+            $field->datatype = 'text';
+            $field->categoryid = $params['categoryid'];
+            $field->locked = 1;
+            $field->visible = 1;
+            $field->param1 = 30;
+            $field->param2 = 2048;
+
+            $field->id = $DB->insert_record('user_info_field', $field);
+        }
+
+        return $field;
+    }
+
 }
+
 ?>
--- lang/en/block_proctoru.php
+++ lang/en/block_proctoru.php
@@ -5,4 +5,6 @@ $string['block_proctoru_roleselectionlabel'] = 'Roles Exempt';
 $string['block_proctoru_roleselectiondescription'] = 'which roles should be excluded from the PU lookup';
 $string['block_proctoru_cron_run']  = 'Cron';
 $string['block_proctoru_cron_desc'] = 'Run with Cron?';
+$string['infofield_shortname'] = "user_proctoru";
+$string['user_proctoru'] = "ProctorU registration status";
 ?>
